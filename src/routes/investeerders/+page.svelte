2c908169f54ebdb1daecb6b542f08ac74accb1cf
TeunvanLith <59139525+TeunvanLith@users.noreply.github.com>
2025-03-17 12:20:04 +0100

    Update +page.svelte

--- src/routes/investeerders/+page.svelte
+++ src/routes/investeerders/+page.svelte
@@ -1,1 +1,20 @@
-<h1>Investeerders</h1>
+<script>
+    import HeroImage from '@/components/HeroImage.svelte';
+    import PageHeading from '@/components/PageHeading.svelte';
+ </script>
+
+<HeroImage 
+imageUrl="https://procalor.nl/wp-content/uploads/2024/09/Sfeer-ProCalor.jpg" 
+bottomEffect="wave" 
+effectColor="green"
+height="300px"
+/>
+
+<main class="max-w-[1400px] mx-auto">
+    <PageHeading 
+  title="Werken met ProCalor" 
+  subtitle="Samen voor een duurzame toekomst" 
+  align="left"
+  className="mt-8"
+/>
+</main>
