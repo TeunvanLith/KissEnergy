ac562586f762ce38a7dd53e374fd3ef390656877
TeunvanLith <59139525+TeunvanLith@users.noreply.github.com>
2025-03-17 12:22:28 +0100

    Update +page.svelte

--- src/routes/gemeenten/+page.svelte
+++ src/routes/gemeenten/+page.svelte
@@ -1,1 +1,20 @@
-<h1>Gemeenten</h1>
+<script>
+    import HeroImage from '@/components/HeroImage.svelte';
+    import PageHeading from '@/components/PageHeading.svelte';
+ </script>
+
+<HeroImage 
+imageUrl="https://procalor.nl/wp-content/uploads/2024/09/Sfeer-ProCalor.jpg" 
+bottomEffect="wave" 
+effectColor="green"
+height="300px"
+/>
+
+<main class="max-w-[1400px] mx-auto">
+    <PageHeading 
+  title="Wij staan voor u klaar" 
+  subtitle="Samen voor een duurzame toekomst" 
+  align="left"
+  className="mt-8"
+/>
+</main>
